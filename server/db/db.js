5c47b17c3f20ff07dccdbfed743464f289b06a14
Pavan Emani <pavanemani14@gmail.com>
2024-12-12 11:34:36 +0530

    data expiration

--- server/db/db.js
+++ server/db/db.js
@@ -3,6 +3,11 @@ const mongoose = require("mongoose");
 const db = mongoose.Schema({
     _id: String,
     data: Object,
+    createdAt: { 
+        type: Date, 
+        default: Date.now,
+        index: { expires: '3d' }
+      },
 })
 
 module.exports = mongoose.model("db", db);
