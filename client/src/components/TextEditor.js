1178ff9937f4d13fdaffee07f2b366d9bb25b75a
Pavan Emani <pavanemani14@gmail.com>
2024-12-12 10:51:15 +0530

    deploy

--- client/src/components/TextEditor.js
+++ client/src/components/TextEditor.js
@@ -24,7 +24,7 @@ export default function TextEditor() {
   const [quill, setQuill] = useState();
 
   useEffect(() => {
-    const s = io("http://localhost:3001");
+    const s = io("https://collabwrite.onrender.com");
     setSocket(s);
 
     return () => {
